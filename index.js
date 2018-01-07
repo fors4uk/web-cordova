7be9dd88a06316c0a5c1da778cc4bc18c0a5df67
fors4uk <andreynesen@gmail.com>
2018-01-07 17:48:26 +0200

    UPDATED
    LAST VESRION

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 $(function(){
     var startCameraAbove = function(){
                 CameraPreview.startCamera({
-                tapEnabled: false,
-                toBack: false,
-                previewDrag: false,
-                tapPhoto: false});
+                    toBack: false,
+                    tapPhoto: false
+                });
                 takePicture();
     };
 
     var stopCamera = function(){
-                setTimeout(function(){
-                    CameraPreview.stopCamera()},
-                    6000);
+                setTimeout(CameraPreview.stopCamera, 6000);
     };
 
     var takePicture = function(){
-               setTimeout(function() {CameraPreview.takePicture({
-               width : 640 ,
-               height : 640 ,
-               quality : 85 },
-                    function ( base64PictureData ){
-                            imageData = 'data:image/jpeg;base64,' + base64PictureData;
-                            document.getElementById('originalPicture').src = imageData;
-                            });
+               setTimeout(function() {
+                   CameraPreview.takePicture(
+                        function ( base64PictureData ){
+                                imageData = 'data:image/jpeg;base64,' + base64PictureData;
+                                document.getElementById('originalPicture').src = imageData;
+                        }
+                    );
                 },3000);
                 stopCamera();
-                };
+    };
 
-    document.getElementById('capturePhoto').addEventListener('click', startCameraAbove, false);
+    $(capturePhoto).on('click', startCameraAbove);
 });
